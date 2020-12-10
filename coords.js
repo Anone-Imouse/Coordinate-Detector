43ca81944ad6dd4696018bf574567146176bb69d
Anone-Imouse <73369548+Anone-Imouse@users.noreply.github.com>
2020-12-09 20:11:31 -0500

    Update coords.js

--- coords.js
+++ coords.js
@@ -7,3 +7,4 @@ const coordsText="Coords: X: "+e.clientY+" Y: "+e.clientX;
  coordsH3.textContent=coordsText;
 });
 });
+//This is my first experience with jQuery on Github, I really love it. I posted my expermients with it on Codepen at https://codepen.io/anone-imouse/pen/eYddpJP for more but I also have this on Codepen at: https://codepen.io/anone-imouse/pen/eYddPZv
